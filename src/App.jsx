7628ad9b36931aa43477c556bbcafc6a6db0aad7
grinjk0418 <grinjk0418@gmail.com>
2025-09-04 17:07:35 +0900

    250904 Conditional Rendering - Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,38 @@
-import { useState } from 'react';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import ProductList from './components/ProductList.jsx';
+import TabUi from './components/TabUi.jsx';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const title = 'ㅁㄴㅇㄻㄴㅇㅎㄹ';
+  const [prodFlg, setProdFlg] = useState(true);
+  const [tabFlg, setTabFlg] = useState(false);
 
-  const titleStyle = {
-    color: 'blue',
-    fontSize: '1rem'
+  const viewProductList = () => {
+    setProdFlg(true);
+    setTabFlg(false);
   }
 
-  const [count, setCount] = useState(0);
-
-  const incrementCount = () => {
-    setCount((prev) => prev + 1);
-  }
-
-    const deincrementCount = () => {
-    setCount((prev) => {
-        if(prev > 0) {
-          return prev -1;
-        }else {
-          return 0;
-        }
-  });
-}
-
-  const [account,setAccount] = useState('');
-
-
-  const [userInfo, setUserInfo] = useState({
-    name: '홍길동',
-    age: 20,
-    gender: 'M'
-  });
-
-  // 기존 State !== 새State 일때만 리렌더링 처리 한다
-
-  const addAge = () => {
-    const copy = {...userInfo};
-    copy.age += 1;
-    setUserInfo(copy);
+  const viewTabUi = () => {
+    setProdFlg(false);
+    setTabFlg(true);
   }
 
   return (
     <>
-      <span>{`${userInfo.name} : ${userInfo.age} : ${userInfo.gender}`}</span>
-      <button type='button' onClick={addAge}>나이 증가</button>
-
-      <br />
-      <br />
-    <input type="text" value={account} onChange={(e) => { setAccount(e.target.value) }}/>
-      <p>{account}</p>
-
-      <p>{count}</p>
-      {/* <button type='button' onClick={() => {setCount(count + 1)}}>+</button> */}
-      {/* 이전값 필요할땐 아래방법.. 위에방식은 광클시 버추얼돔 특성땜에 오류 날수 있다  */}
-      {/* <button type='button' onClick={() => {setCount((prev) => prev + 1)}}>+</button> */}
-      <button type='button' onClick={incrementCount}>+</button>
-      <button type='button' onClick={deincrementCount}>-</button>
-
-    {/* <h1 style="color: red; font-size: 2rem;"></h1> */}
-      <h1 className='title' style={{color: 'red', fontSize: '3rem'}}>{title}</h1>
-      <h1 style={titleStyle}>{title}</h1>
-
+      <Header></Header>
+      <main>
+        <div>
+          <span onClick={viewProductList}>상품 리스트</span>
+          <span onClick={viewTabUi}>테스트</span>
+        </div>
+        { tabFlg && <TabUi></TabUi> }
+        { prodFlg && <ProductList></ProductList> }  
+      </main>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App;
+export default App; //내보낼때 디폴트 적으면 메인에서 임포트 할때 다른이름으로 받을수 있다.
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,62 @@
+import './ProductList.css';
+
+function ProductList() {
+  const products = [
+    {id: 0, title: '바지', info: '좋은 바지', price: 10000, img: 'https://picsum.photos/id/40/4106/2806'},
+    {id: 1, title: '티셔츠', info: '좋은 티셔츠', price: 5000, img: 'https://picsum.photos/id/41/1280/805'},
+    {id: 2, title: '양말', info: '좋은 양말', price: 1000, img: 'https://picsum.photos/id/42/3456/2304'},
+  ];
+
+  return (
+    <div className='card-container'>
+      {
+        products.map((item) => {
+          return (
+            <div className='card' key={item.id}>
+              <div className="card-img" style={{backgroundImage: `url('${item.img}')`}}></div>
+              <p className="card-title">{item.title}</p>
+              <p className="card-price">{item.price}</p>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default ProductList;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   return (
+//     <div className="card-container">
+//       <div className="card">
+//         <div className="card-img" style={{backgroundImage: `url(https://picsum.photos/id/7/4728/3168)`}}></div>
+//         <p className="card-title">바지</p>
+//         <p className="card-price">10000</p>
+//       </div>
+//       <div className="card">
+//         <div className="card-img" style={{backgroundImage: `url(https://picsum.photos/id/7/4728/3168)`}}></div>
+//         <p className="card-title">바지</p>
+//         <p className="card-price">10000</p>
+//       </div>
+//       <div className="card">
+//         <div className="card-img" style={{backgroundImage: `url(https://picsum.photos/id/7/4728/3168)`}}></div>
+//         <p className="card-title">바지</p>
+//         <p className="card-price">10000</p>
+//       </div>
+//     </div>
+//   )
+// }
